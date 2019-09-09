252117e953c29c2c36934f8752cc6ba7bc63cd21
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2019-09-09 04:17:58 +0200

    gtag added

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -16,6 +16,15 @@
 
     <!-- Fonts -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147432471-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-147432471-1');
+    </script>
 </head>
 <body>
 
