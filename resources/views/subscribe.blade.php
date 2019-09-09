1f2c2ccd2f4f59724157a17696dcdd0066e26e6d
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2019-09-09 03:06:31 +0200

    meta updated for live

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -3,7 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="{{ __('backbone.app.description') }}">
+    <meta name="author" content=" Muhammad Sumon Molla Selim">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
     <title>{{ __('backbone.app.name') }} - {{ __('backbone.app.description') }}</title>
 
